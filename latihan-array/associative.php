ed1248494b33afbb8dd810368905adbf0023c8ce
rasyidinazhari <wisnurasyidinazhari99@gmail.com>
2025-04-24 00:52:20 +0700

    Create associative.php

---
+++ latihan-array/associative.php
@@ -0,0 +1,9 @@
+<?php
+$hari = array("senin", "selasa", "rabu", "kamis", "jumat", "sabtu");
+$bulan = ["januari", "februari", "maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "desember"];
+$arr = [100, "teks", true];
+
+//menampilkan array dengan var_dump() dan juga print_r()
+
+
+?>
