579663cd3fd897b674f8d1235bf8f48bfa4fe6a6
rasyidinazhari <wisnurasyidinazhari99@gmail.com>
2025-04-24 13:18:42 +0700

    Update associative.php

--- latihan-array/associative.php
+++ latihan-array/associative.php
@@ -5,5 +5,44 @@ $arr = [100, "teks", true];
 
 //menampilkan array dengan var_dump() dan juga print_r()
 
-
+$mahasiswa =  [
+    [
+        "nama" => "Wisnu Rasyidin Azhari",
+        "nim" => "2110817210010",
+        "email" => "jurusan",
+        "jurusan" => "Teknik Informatika",
+    ],
+    [
+        "nama" => "Rizky Maulana",
+        "nim" => "2110817210011",
+        "email" => "jurusan",
+        "jurusan" => "Teknik Informatika",
+    ],
+    [
+        "nama" => "Muhammad Rizki",
+        "nim" => "2110817210012",
+        "email" => "jurusan",
+        "jurusan" => "Teknik Informatika",
+    ]
+]
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa tapi pake assosiative</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($mahasiswa as $mhs): ?>
+            <li>Nama : <?= $mhs["nama"]?></li>
+            <li>nim : <?= $mhs["nim"]?></li>
+            <li>email : <?= $mhs["email"]?></li>
+            <li>jurusan : <?= $mhs["jurusan"]?></li>
+            <br>
+        <?php endforeach;?>
+    </ul>
+</body>
+</html>
