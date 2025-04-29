b8dac95bb477916224f149ea8444ea395717e995
rasyidinazhari <wisnurasyidinazhari99@gmail.com>
2025-04-29 12:19:17 +0700

    post

---
+++ latihan-getpost/postform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="postaction.php" method="post">
+        <label for="nama">Nama: </label>
+        <input type="text" name="nama" id="nama" required><br><br>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
